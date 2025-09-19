db080f40c4866d6d43639f7fc6dff9b7e2512e3b
jaydipkavar <pateljaydeep1624@gmail.com>
2025-09-19 14:51:53 +0530

    change bg color red-300

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -2,7 +2,7 @@ import LoginForm from "@/components/LoginForm";
 
 function Login() {
   return (
-    <div className="min-h-screen bg-gray-50 flex items-center justify-center p-4">
+    <div className="min-h-screen bg-red-300 flex items-center justify-center p-4">
       <div className="w-full max-w-md">
         <div className="bg-white rounded-lg border border-gray-200 shadow-sm">
           <LoginForm />
